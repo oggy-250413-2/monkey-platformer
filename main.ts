0008134d8aa8b17ff04e9f1473ddb0c0e66729c2
oggy-250413-2 <142149970+oggy-250413-2@users.noreply.github.com>
2023-12-12 20:22:39 +0700

    Update pxt.json, main.blocks, main.ts, pxt.json

--- main.ts
+++ main.ts
@@ -1,121 +1,105 @@
-scene.onOverlapTile(SpriteKind.Projectile, assets.tile`
-        poison pit
-    `, function on_overlap_tile(sprite: Sprite, location: tiles.Location) {
+scene.onOverlapTile(SpriteKind.Projectile, assets.tile`poison pit`, function (sprite, location) {
     tiles.setWallAt(location, false)
-    tiles.setTileAt(location, assets.tile`
-        transparency16
-    `)
+    tiles.setTileAt(location, assets.tile`transparency16`)
 })
-controller.up.onEvent(ControllerButtonEvent.Pressed, function on_up_pressed() {
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     simplified.gravity_jump(mySprite, 50)
-    animation.runImageAnimation(mySprite, assets.animation`
-        jump
-    `, 150, false)
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`jump`,
+    150,
+    false
+    )
 })
-controller.B.onEvent(ControllerButtonEvent.Pressed, function on_b_pressed() {
-    
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
         `, mySprite, 0, 0)
     projectile.setFlag(SpriteFlag.GhostThroughWalls, true)
-    animation.runImageAnimation(projectile, assets.animation`
-        splode
-    `, 100, false)
+    animation.runImageAnimation(
+    projectile,
+    assets.animation`splode`,
+    100,
+    false
+    )
 })
-controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     tiles.setWallAt(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Bottom), true)
-    tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Bottom), assets.tile`
-            bounce
-        `)
+    tiles.setTileAt(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Bottom), assets.tile`bounce`)
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
-    animation.runImageAnimation(mySprite, assets.animation`
-            walk left
-        `, 150, false)
+scene.onOverlapTile(SpriteKind.Projectile, assets.tile`bounce`, function (sprite8, location7) {
+    tiles.setWallAt(location7, false)
+    tiles.setTileAt(location7, assets.tile`transparency16`)
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`
-        orange bauble
-    `, function on_overlap_tile2(sprite2: Sprite, location2: tiles.Location) {
-    
-    tiles.setTileAt(location2, assets.tile`
-        transparency16
-    `)
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`walk left`,
+    150,
+    false
+    )
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`orange bauble`, function (sprite2, location2) {
+    tiles.setTileAt(location2, assets.tile`transparency16`)
     score += 1
 })
-controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
-    animation.runImageAnimation(mySprite, assets.animation`
-            walk right
-        `, 150, false)
+scene.onOverlapTile(SpriteKind.Player, assets.tile`poison pit`, function (sprite7, location6) {
+    game.over(false, effects.melt)
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function on_on_overlap(sprite3: Sprite, otherSprite: Sprite) {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite3, otherSprite) {
     otherSprite.destroy()
     info.changeLifeBy(-1)
 })
-scene.onOverlapTile(SpriteKind.Projectile, assets.tile`
-        skyblock
-    `, function on_overlap_tile3(sprite4: Sprite, location3: tiles.Location) {
-    tiles.setWallAt(location3, false)
-    tiles.setTileAt(location3, assets.tile`
-        transparency16
-    `)
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`walk right`,
+    150,
+    false
+    )
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`
-        chest2
-    `, function on_overlap_tile4(sprite5: Sprite, location4: tiles.Location) {
-    game.over(true, effects.confetti)
-})
-scene.onOverlapTile(SpriteKind.Player, assets.tile`
-        chest1
-    `, function on_overlap_tile5(sprite6: Sprite, location5: tiles.Location) {
-    scene.setBackgroundImage(assets.image`
-        background2
-    `)
-    tiles.setTilemap(tilemap`
-        level2
-    `)
-    animation.runMovementAnimation(mySprite, animation.animationPresets(animation.flyToCenter), 2000, false)
+scene.onOverlapTile(SpriteKind.Player, assets.tile`chest1`, function (sprite6, location5) {
+    scene.setBackgroundImage(assets.image`background2`)
+    tiles.setTilemap(tilemap`level2`)
+    animation.runMovementAnimation(
+    mySprite,
+    animation.animationPresets(animation.flyToCenter),
+    2000,
+    false
+    )
     mySprite.say("Level 2!", 500)
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`
-        poison pit
-    `, function on_overlap_tile6(sprite7: Sprite, location6: tiles.Location) {
+scene.onOverlapTile(SpriteKind.Player, assets.tile`chest2`, function (sprite5, location4) {
+    game.over(true, effects.confetti)
+})
+scene.onOverlapTile(SpriteKind.Projectile, assets.tile`skyblock`, function (sprite4, location3) {
+    tiles.setWallAt(location3, false)
+    tiles.setTileAt(location3, assets.tile`transparency16`)
+})
+info.onLifeZero(function () {
     game.over(false, effects.melt)
 })
-scene.onOverlapTile(SpriteKind.Projectile, assets.tile`
-        bounce
-    `, function on_overlap_tile7(sprite8: Sprite, location7: tiles.Location) {
-    tiles.setWallAt(location7, false)
-    tiles.setTileAt(location7, assets.tile`
-        transparency16
-    `)
-})
-let projectile : Sprite = null
-let mySprite : Sprite = null
 let score = 0
-scene.setBackgroundImage(assets.image`
-    background
-`)
-tiles.setTilemap(tilemap`
-    level1
-`)
-mySprite = sprites.create(assets.image`
-    stand
-`, SpriteKind.Player)
+let projectile: Sprite = null
+let mySprite: Sprite = null
+scene.setBackgroundImage(assets.image`background`)
+tiles.setTilemap(tilemap`level1`)
+mySprite = sprites.create(assets.image`stand`, SpriteKind.Player)
 controller.moveSprite(mySprite, 100, 0)
 mySprite.ay = 50
 scene.cameraFollowSprite(mySprite)
